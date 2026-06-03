7a60e474d5a6f7387e31ac3ff8b1cc3488265ae7
yifanye69-droid <yifanye69@gmail.com>
2026-06-03 10:09:59 +0800

    恢复Vercel配置

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
   images: {
-    unoptimized: true,
+    formats: ["image/avif", "image/webp"],
   },
-  basePath: '/interactive-map-site',
-  assetPrefix: '/interactive-map-site',
 };
 
 export default nextConfig;
